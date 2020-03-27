c78b6cba84a6f087838ff3f8a79d462ffe82f0b0
Jenna Goldman <jenna.goldman89@gmail.com>
2020-03-27 11:29:46 -0700

    add POST route for creating an actor, and associated test

---
+++ __tests__/actors.test.js
@@ -0,0 +1,25 @@
+const { getActor, getActors } = require('../db/data-helpers');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('actor routes', () => {
+  it('creates a new actor', () => {
+    return request(app)
+      .post('/api/v1/actors')
+      .send({
+        name: 'Jenna Goldman',
+        pob: 'Los Gatos, CA'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Jenna Goldman',
+          pob: 'Los Gatos, CA',
+          __v: 0
+        });
+      });
+
+  });
+})
+;
